586ef56a959d91d334eb19e3d65569d6eeb0f6e1
Alexandr Morozov <lk4d4math@gmail.com>
2014-08-28 15:17:51 +0400

    Initial commit
    Signed-off-by: Alexandr Morozov <lk4d4math@gmail.com>

---
+++ lock.go
@@ -0,0 +1,68 @@
+package debugmutex
+
+import (
+	"fmt"
+	"log"
+	"runtime"
+	"runtime/debug"
+	"sync"
+	"time"
+)
+
+// Mutex is debug mutex, which prints logs and traces for locks
+// implements sync.Locker interface
+type Mutex struct {
+	retries  int
+	mu       sync.Mutex
+	myMu     sync.RWMutex
+	lockedAt string
+}
+
+// New returns new debug mutex
+// retries parameter specify number of retries acquiring Mutex before fatal
+// exit, if <=0, then wait forever
+func New(retries int) *Mutex {
+	return &Mutex{retries: retries}
+}
+
+func (m *Mutex) Lock() {
+	_, file, line, _ := runtime.Caller(1)
+	caller := fmt.Sprintf("%s:%d", file, line)
+	log.Printf("Trying to acquire Lock at %s", caller)
+	wait := make(chan struct{})
+	go func() {
+		m.mu.Lock()
+		close(wait)
+	}()
+	seconds := 0
+loop:
+	for {
+		select {
+		case <-wait:
+			break loop
+		case <-time.After(1 * time.Second):
+			m.myMu.RLock()
+			seconds++
+			if m.retries > 0 && seconds > m.retries {
+				debug.PrintStack()
+				log.Fatalf("Possible deadlock - can't acquire lock at %s for 5 second, locked by %s", caller, m.lockedAt)
+			}
+			log.Printf("Lock is stuck at %s, wait for lock from %s", caller, m.lockedAt)
+			m.myMu.RUnlock()
+		}
+	}
+	log.Printf("Lock acquired at %s", caller)
+	debug.PrintStack()
+	m.myMu.Lock()
+	m.lockedAt = caller
+	m.myMu.Unlock()
+}
+
+func (m *Mutex) Unlock() {
+	_, file, line, _ := runtime.Caller(1)
+	caller := fmt.Sprintf("%s:%d", file, line)
+	m.myMu.RLock()
+	log.Printf("Release Lock locked at %s, at %s", m.lockedAt, caller)
+	m.myMu.RUnlock()
+	m.mu.Unlock()
+}
