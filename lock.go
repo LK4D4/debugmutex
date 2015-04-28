fbb155c51cdd7b35bf0edb5546fdbe7155467700
Alexander Morozov <lk4d4@docker.com>
2015-04-28 12:50:49 -0700

    Use logrus and add fatal option
    Signed-off-by: Alexander Morozov <lk4d4@docker.com>

--- lock.go
+++ lock.go
@@ -2,11 +2,11 @@ package debugmutex
 
 import (
 	"fmt"
-	"log"
 	"runtime"
-	"runtime/debug"
 	"sync"
 	"time"
+
+	"github.com/Sirupsen/logrus"
 )
 
 // Mutex is debug mutex, which prints logs and traces for locks
@@ -16,13 +16,23 @@ type Mutex struct {
 	mu       sync.Mutex
 	myMu     sync.RWMutex
 	lockedAt string
+	errFunc  func(format string, args ...interface{})
 }
 
 // New returns new debug mutex
-// retries parameter specify number of retries acquiring Mutex before fatal
-// exit, if <=0, then wait forever
-func New(retries int) sync.Locker {
-	return &Mutex{retries: retries}
+// retries parameter specify number of retries acquiring Mutex before error
+// message, if <=0, then wait forever
+// if fatal is true, then program will exit by os.Exit(1)
+func New(retries int, fatal bool) sync.Locker {
+	var errF func(format string, args ...interface{})
+	errF = logrus.Errorf
+	if fatal {
+		errF = logrus.Fatalf
+	}
+	return &Mutex{
+		retries: retries,
+		errFunc: errF,
+	}
 }
 
 // Lock tries to lock mutex. Interval between attempts is 1 second.
@@ -31,7 +41,7 @@ func New(retries int) sync.Locker {
 func (m *Mutex) Lock() {
 	_, file, line, _ := runtime.Caller(1)
 	caller := fmt.Sprintf("%s:%d", file, line)
-	log.Printf("Trying to acquire Lock at %s", caller)
+	logrus.Debugf("Trying to acquire Lock at %s", caller)
 	wait := make(chan struct{})
 	go func() {
 		m.mu.Lock()
@@ -47,15 +57,15 @@ loop:
 			m.myMu.RLock()
 			seconds++
 			if m.retries > 0 && seconds > m.retries {
-				debug.PrintStack()
-				log.Fatalf("Possible deadlock - can't acquire lock at %s for 5 second, locked by %s", caller, m.lockedAt)
+				logrus.Errorf("Stack:\n%s", stack())
+				m.errFunc("Possible deadlock - can't acquire lock at %s for 5 second, locked by %s", caller, m.lockedAt)
 			}
-			log.Printf("Lock is stuck at %s, wait for lock from %s", caller, m.lockedAt)
+			logrus.Debugf("Lock is stuck at %s, wait for lock from %s", caller, m.lockedAt)
 			m.myMu.RUnlock()
 		}
 	}
-	log.Printf("Lock acquired at %s", caller)
-	debug.PrintStack()
+	logrus.Debugf("Lock acquired at %s", caller)
+	logrus.Debugf("Stack:\n%s", stack())
 	m.myMu.Lock()
 	m.lockedAt = caller
 	m.myMu.Unlock()
@@ -67,7 +77,12 @@ func (m *Mutex) Unlock() {
 	_, file, line, _ := runtime.Caller(1)
 	caller := fmt.Sprintf("%s:%d", file, line)
 	m.myMu.RLock()
-	log.Printf("Release Lock locked at %s, at %s", m.lockedAt, caller)
+	logrus.Debugf("Release Lock locked at %s, at %s", m.lockedAt, caller)
 	m.myMu.RUnlock()
 	m.mu.Unlock()
+}
+
+func stack() string {
+	var buf [2 << 10]byte
+	return string(buf[:runtime.Stack(buf[:], false)])
 }
