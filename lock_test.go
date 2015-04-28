7d4989f4074e6adb50200c5e5542a71485e711ac
Alexander Morozov <lk4d4@docker.com>
2015-04-28 12:52:30 -0700

    Add tests
    Signed-off-by: Alexander Morozov <lk4d4@docker.com>

---
+++ lock_test.go
@@ -0,0 +1,42 @@
+package debugmutex
+
+import (
+	"io/ioutil"
+	"strings"
+	"testing"
+
+	"github.com/Sirupsen/logrus"
+)
+
+func init() {
+	logrus.SetOutput(ioutil.Discard)
+}
+
+func TestLockUnlock(t *testing.T) {
+	l := New(1, true)
+	l.Lock()
+	l.Unlock()
+}
+
+func TestDoubleLockPanic(t *testing.T) {
+	l := New(1, true)
+	lock := l.(*Mutex)
+	lock.errFunc = logrus.Panicf
+	l.Lock()
+
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Fatal("Double lock shoud fatal")
+		}
+		e, ok := r.(*logrus.Entry)
+		if !ok {
+			t.Fatalf("Unexpected type from panic: %T", r)
+		}
+		if !strings.Contains(e.Message, "Possible deadlock") {
+			t.Fatalf("Unexpected message from panic: %s", e.Message)
+		}
+	}()
+
+	l.Lock()
+}
